8493d1622b8d3da79c32a0871c30219b6a45e97f
RIYA <jack@jill>
2022-08-26 08:44:07 -0500

    fish

---
+++ src/main/java/com/Nexttech/jack/App.java
@@ -0,0 +1,13 @@
+package com.Nexttech.jack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
